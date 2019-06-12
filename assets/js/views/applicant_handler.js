1f26b6f29fb6e42d65ef32af858f99b6a55392dd
Deepak <deepak@pixlcoders.com>
2019-06-12 18:33:30 +0530

    Signup applicant freshers wiring done

--- assets/js/views/applicant_handler.js
+++ assets/js/views/applicant_handler.js
@@ -8,42 +8,35 @@ function ApplicantSignUpHandler() {
         $('.js_select2').select2({});
         var _form_name = '#jsSignUpApplicantForm';
         var _form      = $(_form_name);
+        console.log(_form);
 
         _form.unbind().submit(function(e) {
             e.preventDefault();
+            console.log(_form_name);
             if(FormValidator.validateForm(_form_name)) {
-                var obj      = {
-                    name        :_form.find('.js_name').val(),
-                    designation :_form.find('.js_designation').val(),
-                    company     :_form.find('.js_company').val(),
-                    cur_salary  :_form.find('.js_cur_salary').val(),
-                    anual_salary:_form.find('.js_anual_salary').val(),
-                    mobile      :_form.find('.js_mobile').val() || '0',
-                    email       :_form.find('.js_email').val(),
-                    password    :_form.find('.js_password').val()
+                var obj = {
+                    name           :_form.find('.js_name').val(),
+                    qualification  :_form.find('.js_qualification').val(),
+                    institution    :_form.find('.js_institution').val(),
+                    designation    :_form.find('.js_designation').val(),
+                    company        :_form.find('.js_company').val(),
+                    current_salary :_form.find('.js_cur_salary').val(),
+                    expected_salary:_form.find('.js_anual_salary').val(),
+                    mobile         :_form.find('.js_mobile').val() || '0',
+                    email          :_form.find('.js_email').val(),
+                    password       :_form.find('.js_password').val(),
+                    gender         :_form.find('.js_gender').val()
                 };
-                ApiUtil.makeAjaxRequest('/api/applicant-auth/register', '', 'POST', '', _obj, function(_res) {
+                console.log(obj);
+                var callback = function(_res) {
                     if(!_res.error) {
-                        window.location.href = '/';
+                        window.location.href = '/'
                     } else {
                         alert(_res.message || 'Something went wrong!');
                     }
-                });
-
-
-
-                //
-                //
-                //var callback = function(_res) {
-                //    if(!_res.error) {
-                //        window.location.href = '/'
-                //    } else {
-                //        alert(_res.message || 'Something went wrong!');
-                //    }
-                //};
-                //ApiUtil.makeAjaxRequest('/api/applicant-auth/register', '', 'POST', '', obj, callback);
+                };
+                ApiUtil.makeAjaxRequest('/api/applicant-auth/register', '', 'POST', '', obj, callback);
             }
-            return false;
         });
     }
 
