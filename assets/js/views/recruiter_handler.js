d84f5f0238e58fbdd54ccc07fcba719c9178b75e
Sunil <sunil@pixlcoders.com>
2019-06-19 12:44:53 +0530

    recruiter signup  wiring done

--- assets/js/views/recruiter_handler.js
+++ assets/js/views/recruiter_handler.js
@@ -3,6 +3,28 @@ var FormValidator = require('../utils/formValidator');
 var utils         = require('../utils/common');
 
 function RecruiterHandler() {
+    function uploadImage(_ele, _cb) {
+        var formData = new FormData();
+        formData.append('photo', _ele[0].files[0]);
+        ApiUtil.makeFileUploadRequest('/api/recruiter/photo/upload', '', 'POST', '', formData,
+            function(_res_path) {
+                _cb(_res_path);
+            });
+    }
+
+    function readURL(input) {
+        if(input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                $('.js_img_pre_holder').attr('src', e.target.result);
+                $('.js_img_pre_holder').removeClass('hide');
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
     function bindRecruiterEvent() {
         $('.js_select2').select2({});
         var _form_name = '#jsSignUpRecruitForm';
@@ -10,50 +32,54 @@ function RecruiterHandler() {
 
         _form.unbind().submit(function(e) {
             e.preventDefault();
+            console.log('clicked');
             if(FormValidator.validateForm(_form_name)) {
-                var obj = {
+                var obj             = {
                     name           :_form.find('.js_name').val(),
                     email          :_form.find('.js_email').val(),
                     password       :_form.find('.js_password').val(),
                     gender         :_form.find('.js_gender').val(),
                     designation    :_form.find('.js_designation').val(),
                     company        :_form.find('.js_company_name').val() || '0',
-                    industry       :_form.find('.js_industry').val(),
+                    industry_id    :_form.find('.js_industry').val(),
                     company_size   :_form.find('.js_company_size').val(),
                     company_url    :_form.find('.js_company_url').val(),
                     about_company  :_form.find('.js_about_company').val(),
                     company_benefit:_form.find('.js_company_benefit').val()
 
                 };
-                console.log(obj);
-                var callback = function(_res) {
-                    if(!_res.error) {
-                        window.location.href = '/recruiter'
-                    } else {
-                        alert(_res.message || 'Something went wrong!');
+                var _img_pre_holder = _form.find('.js_input_profile_file');
+                uploadImage(_img_pre_holder, function(_res_path) {
+                    if(!_res_path.error && _res_path.data) {
+                        obj.photo      = _res_path.data.file;
+                        obj.photo_type = _res_path.data.file_type;
                     }
-                };
-                ApiUtil.makeAjaxRequest('/api/recruiter-auth/register', '', 'POST', '', obj, callback);
+                    ApiUtil.makeAjaxRequest('/api/recruiter-auth/register', '', 'POST', '', obj, function(_res) {
+                        if(!_res.error) {
+                            alert(_res.message);
+                            window.location.href = '/login'
+                        } else {
+                            alert(_res.message || 'Something went wrong!');
+                        }
+                    });
+                })
             }
         });
     }
 
+    function bindCommonClickEvents() {
+        $('.js_input_profile_file').change(function() {
+            readURL(this);
+        });
+        $('.js_company_benefit').select2({
+            tags:true
+        });
+    }
+
     return {
-        init    :function() {
+        init:function() {
+            bindCommonClickEvents();
             bindRecruiterEvent();
-        },
-        initView:function(_land_ids) {
-            _lang_ids_arr = _land_ids;
-            utils.initProfileImageCropper('#profileImageCropperModal', '.js_profile_img_wrap', 'userPicPreviewImage',
-                function() {
-                    $('.js_prof_pic_load_img').removeClass('hide');
-                    ApiUtil.makeAjaxRequest('/api/avatar/upload', '', 'POST', '',
-                        {src:$('.js_img_pre_holder').attr('src')}, function(_res) {
-                            $('.js_prof_pic_load_img').addClass('hide');
-
-                        });
-                });
-
         }
     }
 }
