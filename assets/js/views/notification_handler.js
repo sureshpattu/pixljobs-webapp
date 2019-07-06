d80400bd87daca779706a0572ba6ace43bc5f041
Deepak <deepak@pixlcoders.com>
2019-07-06 13:39:13 +0530

    bug fixes and UI changes

--- assets/js/views/notification_handler.js
+++ assets/js/views/notification_handler.js
@@ -4,8 +4,49 @@ var utils         = require('../utils/common');
 var async         = require('async');
 
 function NotificationHandler() {
+    var _checkedArr = [];
 
     function bindCommonClickEvents() {
+
+        var _check_all_notification = $('.js_check_all_notifications');
+        var _check_notification     = $('.js_check_notification');
+        _check_all_notification.click(function() {
+            if($(this).prop('checked')) {
+                _check_notification.each(function() {
+                    $(this).prop('checked', true);
+                    _checkedArr.push($(this).val());
+                });
+            } else {
+                _check_notification.each(function() {
+                    $(this).prop('checked', false);
+                    var _index = _checkedArr.indexOf($(this).val());
+                    if(_index !== -1) {
+                        _checkedArr.splice(_index, 1);
+                    }
+                });
+                _checkedArr = _.uniq(_checkedArr);
+            }
+        });
+        _check_notification.click(function() {
+            _check_all_notification.prop('checked', false);
+            if($(this).prop('checked')) {
+                _checkedArr.push($(this).val());
+            } else {
+                var _index = _checkedArr.indexOf($(this).val());
+                if(_index !== -1) {
+                    _checkedArr.splice(_index, 1);
+                }
+            }
+        });
+
+        $('.js_check_all_notifications ').change(function() {
+            if($(this).prop('checked')) {
+                $('.js_check_notification.unread').prop('checked', true);
+            } else {
+                $('.js_check_notification.unread').prop('checked', false);
+            }
+        });
+
         $('.js_select2').select2({});
 
         $('.js_input_profile_file').change(function() {
@@ -13,11 +54,11 @@ function NotificationHandler() {
         });
 
         $('.js_panel_title.unread').click(function() {
-            var _this   = $(this);
-            var _notification_id =$(this).data('notification_id');
+            var _this            = $(this);
+            var _notification_id = $(this).data('notification_id');
 
             var _obj = {
-                status:"read"
+                status:'read'
             };
             ApiUtil.makeAjaxRequest('/api/notifications/' + _notification_id, '', 'PUT', '', _obj, function(_res) {
                 if(!_res.error && _res.data) {
