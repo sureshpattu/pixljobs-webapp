ae8a725bd96b44098a89f535615b081684f3d9e3
Deepak <deepak@pixlcoders.com>
2019-07-08 17:56:12 +0530

    Bug fixes

--- routes/index.js
+++ routes/index.js
@@ -345,6 +345,11 @@ router.get('/recruiter', verify.isRecruiterLoggedIn, function(req, res) {
             helper_utils.makeApiRequest(req, 'GET', '/benefits', function(_res) {
                 callback(null, _res);
             });
+        },
+        function(callback) {
+            helper_utils.makeApiRequest(req, 'GET', '/country-code', function(_res) {
+                callback(null, _res);
+            });
         }
     ], function(err, results) {
         var _companies = [];
@@ -352,12 +357,13 @@ router.get('/recruiter', verify.isRecruiterLoggedIn, function(req, res) {
             _companies = results[0].data.companies;
         }
         res.render('recruiter_profile', {
-            companies :_companies,
-            user      :!results[0].error ? results[0].data : [],
-            data      :!results[1].error ? results[1].data : [],
-            industries:!results[2].error ? results[2].data : [],
-            benefits  :!results[3].error ? results[3].data : [],
-            user_id   :req.cookies.pixljob_user_id
+            companies   :_companies,
+            user        :!results[0].error ? results[0].data : [],
+            data        :!results[1].error ? results[1].data : [],
+            industries  :!results[2].error ? results[2].data : [],
+            benefits    :!results[3].error ? results[3].data : [],
+            country_code:!results[4].error ? results[4].data : [],
+            user_id     :req.cookies.pixljob_user_id
         });
     });
 });
@@ -725,7 +731,7 @@ router.get('/applicant/:applicant_id/:application_id', verify.isRecruiterLoggedI
             req.body.status = 'viewed';
             helper_utils.makeApiRequest(req,
                 'PUT',
-                '/job-applications/'  + req.params.application_id,
+                '/job-applications/' + req.params.application_id,
                 function(_res) {
                     callback(null, _res);
                 });
@@ -736,11 +742,11 @@ router.get('/applicant/:applicant_id/:application_id', verify.isRecruiterLoggedI
             is_experience = true;
         }
         res.render('applicant_details', {
-            user        :!results[0].error ? results[0].data : [],
-            data        :!results[1].error ? results[1].data : [],
-            country_code:!results[2].error ? results[2].data : [],
-            applications:!results[3].error ? results[3].data : [],
-            exp         :is_experience,
+            user          :!results[0].error ? results[0].data : [],
+            data          :!results[1].error ? results[1].data : [],
+            country_code  :!results[2].error ? results[2].data : [],
+            applications  :!results[3].error ? results[3].data : [],
+            exp           :is_experience,
             application_id:req.params.application_id
         });
     });
